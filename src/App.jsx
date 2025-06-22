56d20940c30a68da3afc12d7f646e98b9c30fdfd
verakissyou <veronicaciuca17@gmail.com>
2025-06-22 23:55:01 +0300

    Finish the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,57 @@
-
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState({
+    id: "1",
+    advice:
+      "Remember that spiders are more afraid of you, than you are of them.",
+  });
+
+  const getNewQuote = async () => {
+    const response = await fetch(`https://api.adviceslip.com/advice`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    const jsonQuoteData = await response.json();
+
+    setData((prev) => ({
+      ...prev,
+      id: jsonQuoteData.slip.id,
+      advice: jsonQuoteData.slip.advice,
+    }));
+  };
 
   return (
     <>
-    <h1>heloo</h1>
+      <div className="wrapper">
+        <header>
+          <h1>Advice #{data.id}</h1>
+        </header>
+        <main>
+          <article>
+            <h2>&quot;{data.advice}&quot;</h2>
+          </article>
+          <div className="image-wrapper">
+            <picture>
+              <source
+                srcSet="src\images\pattern-divider-desktop.svg"
+                media="(min-width: 500px)"
+              />
+              <img
+                src="src\images\pattern-divider-mobile.svg"
+                alt="pattern-divider"
+              />
+            </picture>
+          </div>
+        </main>
+          <button aria-label="Generate new quote"  type="button" onClick={getNewQuote}>
+            <img src="src\images\icon-dice.svg" alt="generate-quote" />
+          </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
